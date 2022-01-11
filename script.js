bde130e9305402e86aa6d89ac73b0d6f855a863e
Mariamsharif <mariamsharif99@gmail.com>
2022-01-11 23:32:31 +0000

    adding a character length prompt to the page

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var special = ['!','"','#','$','%','&','’','(',')','*','+','=','@','?'];
 
+//Variable Declaration
+var confrimLength = "";
+var confrimSpecailCharacters;
+var confrimNumericCharecters;
+var confirmUpperCase;
+var confirmLowerCase;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword () { 
+ //Validate length 8-128
+
+ var confrimLength = (prompt("The Characters of your passwords needs to be between 8-128."));
+ //if it doesn't meet the requirment
+ while(confrimLength < 8 || confrimLength > 128) {
+   alert("Password length must be between 8-128 characters long.Try again");
+   var confrimLength = (prompt("The Characters of your passwords needs to be between 8-128."));
+ };
 
 }
 
